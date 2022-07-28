e8ada7b503b5d102bb5fedcdb9402994e04ecc59
Bruno Moraes <103973043+BrMoraes@users.noreply.github.com>
2022-07-28 15:24:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+let data = {};
+
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+   console.log("O botão foi clicado");
+
+   formValidation();
+});
+
+let formValidation = () => {
+   if (input.value === "") {
+      msg.innerHTML = "A postagem não pode ser em branco";
+      console.log("Falhou");
+   } else {
+      console.log("Sucesso");
+      msg.innerHTML = "";
+      acceptData();
+   }
+}
+
+let acceptData = () => {
+   data["text"] = input.value;
+   console.log(data);
+   createPost();
+};
+
+let createPost = () => {
+   posts.innerHTML += `
+   <div>
+      <p>${data.text}</p>
+      <span class="options">
+         <i onClick="editPost(this)" class="fas fa-edit"></i>
+         <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+   </div>
+   `;
+   input.value = "";
+};
+
+let deletePost = (e) => {
+   e.parentElement.parentElement.remove();
+}
+
+let editPost = (e) => {
+   input.value = e.parentElement.previousElementSibling.innerHTML;
+   e.parentElement.parentElement.remove();
+}
